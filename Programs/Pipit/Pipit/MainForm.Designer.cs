1602ba596ff4b9bdb90fb74baf6b316c9a7e863d
gfutia <gfutia@1a4e458a-7fe6-4c0f-a286-c717e70bd3bd>
2010-10-12 16:39:24 +0000

    git-svn-id: https://helmholtz.ucdenver.pvt/svn/Software/C%20Sharp@657 1a4e458a-7fe6-4c0f-a286-c717e70bd3bd

---
+++ Programs/Pipit/Pipit/MainForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Pipit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ao0numeric = new System.Windows.Forms.NumericUpDown();
+            this.ao1numeric = new System.Windows.Forms.NumericUpDown();
+            this.ao2numeric = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.setVoltageButton = new System.Windows.Forms.Button();
+            this.deviceComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ao0numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ao1numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ao2numeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ao0numeric
+            // 
+            this.ao0numeric.DecimalPlaces = 4;
+            this.ao0numeric.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.ao0numeric.Location = new System.Drawing.Point(12, 92);
+            this.ao0numeric.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.ao0numeric.Name = "ao0numeric";
+            this.ao0numeric.Size = new System.Drawing.Size(93, 20);
+            this.ao0numeric.TabIndex = 0;
+            // 
+            // ao1numeric
+            // 
+            this.ao1numeric.DecimalPlaces = 4;
+            this.ao1numeric.Location = new System.Drawing.Point(12, 134);
+            this.ao1numeric.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.ao1numeric.Name = "ao1numeric";
+            this.ao1numeric.Size = new System.Drawing.Size(93, 20);
+            this.ao1numeric.TabIndex = 1;
+            // 
+            // ao2numeric
+            // 
+            this.ao2numeric.DecimalPlaces = 4;
+            this.ao2numeric.Location = new System.Drawing.Point(12, 179);
+            this.ao2numeric.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.ao2numeric.Name = "ao2numeric";
+            this.ao2numeric.Size = new System.Drawing.Size(93, 20);
+            this.ao2numeric.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Analog Out 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Analog Out 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Analog Out 2";
+            // 
+            // setVoltageButton
+            // 
+            this.setVoltageButton.Location = new System.Drawing.Point(12, 219);
+            this.setVoltageButton.Name = "setVoltageButton";
+            this.setVoltageButton.Size = new System.Drawing.Size(75, 23);
+            this.setVoltageButton.TabIndex = 6;
+            this.setVoltageButton.Text = "Set Voltage";
+            this.setVoltageButton.UseVisualStyleBackColor = true;
+            this.setVoltageButton.Click += new System.EventHandler(this.setVoltageButton_Click);
+            // 
+            // deviceComboBox
+            // 
+            this.deviceComboBox.FormattingEnabled = true;
+            this.deviceComboBox.Location = new System.Drawing.Point(12, 40);
+            this.deviceComboBox.Name = "deviceComboBox";
+            this.deviceComboBox.Size = new System.Drawing.Size(121, 21);
+            this.deviceComboBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Device";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 265);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.deviceComboBox);
+            this.Controls.Add(this.setVoltageButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ao2numeric);
+            this.Controls.Add(this.ao1numeric);
+            this.Controls.Add(this.ao0numeric);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Pipit";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.ao0numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ao1numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ao2numeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown ao0numeric;
+        private System.Windows.Forms.NumericUpDown ao1numeric;
+        private System.Windows.Forms.NumericUpDown ao2numeric;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button setVoltageButton;
+        private System.Windows.Forms.ComboBox deviceComboBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Programs/Pipit/Pipit/MainForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using OpenLayers.Base;
+
+namespace Pipit
+{
+    public partial class MainForm : Form
+    {
+        DeviceMgr deviceMgrObj = null;
+        Device daqDevice = null;
+        AnalogOutputSubsystem aoSubObj = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            deviceMgrObj = DeviceMgr.Get();     // first need to get the device manager
+            
+            string[] deviceNames = deviceMgrObj.GetDeviceNames();   // then get the names of the devices
+            deviceComboBox.Items.AddRange(deviceNames);             // and populate them into the device combo box
+
+            
+            if (deviceComboBox.Items.Count > 0)
+            {
+                deviceComboBox.SelectedIndex = 0;
+
+                InitalizeDTDAQ();
+            }
+        }
+
+        public void InitalizeDTDAQ()
+        {
+            // this can only be done if there was a device detected
+            // there was a device detected of the device combo box is populated
+            if (deviceComboBox.Items.Count <= 0)
+                return; // no device detected just return
+
+            // if there was already a AO subsystem device dispose of it
+            if (aoSubObj != null)
+            {
+                aoSubObj.Dispose();
+                aoSubObj = null;
+            }
+
+            // if there was already a daq device dispose of it
+            if (daqDevice != null)
+            {
+                daqDevice.Dispose();
+                daqDevice = null;
+            }
+
+
+            daqDevice = deviceMgrObj.GetDevice((string) deviceComboBox.SelectedItem);
+
+            // check if this device has an analog out subsystem
+            if (daqDevice.GetNumSubsystemElements(SubsystemType.AnalogOutput) == 0)
+            {
+                // this device has no analog output subsystems
+                Exception ex = new Exception("DT Device Error: Not Analog Output on Device");
+                throw(ex);
+            }
+
+            // get the first subsystem
+            aoSubObj = daqDevice.AnalogOutputSubsystem(0);
+
+            aoSubObj.DataFlow = DataFlow.SingleValue;
+            aoSubObj.ChannelType = ChannelType.SingleEnded;
+            aoSubObj.Config();
+
+            // if the device has less than 3 channels 
+            // enable the numbe of channels numeric only for the number of channels in use
+            if (aoSubObj.NumberOfChannels <= 3)
+            {
+                if (aoSubObj.NumberOfChannels <= 0)
+                {
+                    ao0numeric.Enabled = false;
+                    ao1numeric.Enabled = false;
+                    ao2numeric.Enabled = false;
+                }
+                else
+                {
+                    switch(aoSubObj.NumberOfChannels)
+                    {
+                        case 1:
+                            ao0numeric.Enabled = true;
+                            ao1numeric.Enabled = false;
+                            ao2numeric.Enabled = false;
+                            break;
+
+                        case 2:
+                            ao0numeric.Enabled = true;
+                            ao1numeric.Enabled = true;
+                            ao2numeric.Enabled = false;
+                            break;
+
+                        case 3:
+                        default:
+                            ao0numeric.Enabled = true;
+                            ao1numeric.Enabled = true;
+                            ao2numeric.Enabled = true; 
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                ao0numeric.Enabled = true;
+                ao1numeric.Enabled = true;
+                ao2numeric.Enabled = true; 
+            }
+
+            // update the voltage values
+            setVoltageButton_Click(this, null);
+        }
+
+        public void SetDAQVoltages(double []chVolts)
+        {
+            int numChannels = aoSubObj.NumberOfChannels;
+
+            // iterate through the volts loop to get the channels
+            for (int i = 0; i < chVolts.Length && i < numChannels; i++)
+            {
+                aoSubObj.SetSingleValueAsVolts(i, chVolts[i]);
+            }
+        }
+
+        public void Exit()
+        {
+            if (daqDevice != null)
+            {
+                daqDevice.Dispose();
+                daqDevice = null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Exit();
+        }
+
+        private void setVoltageButton_Click(object sender, EventArgs e)
+        {
+            // populate the array with the voltage values
+            // don't worry about if the channel doesn't exist
+            // that problem is dealt with in the voltage output
+            double[] volts = new double[3];
+
+            volts[0] = Convert.ToDouble(ao0numeric.Value);
+            volts[1] = Convert.ToDouble(ao1numeric.Value);
+            volts[2] = Convert.ToDouble(ao2numeric.Value);
+
+            SetDAQVoltages(volts);
+        }
+    }
+}
