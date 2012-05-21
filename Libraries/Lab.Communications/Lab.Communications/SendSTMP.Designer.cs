cca3e64f551ce9416c8a7cb14d430835c9d14108
gfutia <gfutia@1a4e458a-7fe6-4c0f-a286-c717e70bd3bd>
2012-05-21 01:43:27 +0000

    New SendSTMP project. Use to send text messages.
    git-svn-id: https://helmholtz.ucdenver.pvt/svn/Software/C%20Sharp@1092 1a4e458a-7fe6-4c0f-a286-c717e70bd3bd

---
+++ Libraries/Lab.Communications/Lab.Communications/SendSTMP.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Lab.Communications
+{
+    partial class SendSTMP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.recipentTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverTextBox = new System.Windows.Forms.TextBox();
+            this.sendEmail = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.subjectTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Recipent";
+            // 
+            // recipentTextBox
+            // 
+            this.recipentTextBox.Location = new System.Drawing.Point(17, 124);
+            this.recipentTextBox.Name = "recipentTextBox";
+            this.recipentTextBox.Size = new System.Drawing.Size(297, 20);
+            this.recipentTextBox.TabIndex = 7;
+            this.recipentTextBox.Text = "3307601541@txt.att.net";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "My Message";
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(17, 220);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(297, 20);
+            this.messageTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "STMP Server";
+            // 
+            // serverTextBox
+            // 
+            this.serverTextBox.Location = new System.Drawing.Point(17, 30);
+            this.serverTextBox.Name = "serverTextBox";
+            this.serverTextBox.Size = new System.Drawing.Size(259, 20);
+            this.serverTextBox.TabIndex = 3;
+            this.serverTextBox.Text = "mail.gregfutia.com";
+            // 
+            // sendEmail
+            // 
+            this.sendEmail.Location = new System.Drawing.Point(17, 264);
+            this.sendEmail.Name = "sendEmail";
+            this.sendEmail.Size = new System.Drawing.Size(75, 23);
+            this.sendEmail.TabIndex = 2;
+            this.sendEmail.Text = "Send E-mail";
+            this.sendEmail.UseVisualStyleBackColor = true;
+            this.sendEmail.Click += new System.EventHandler(this.sendEmail_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(288, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Port";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "26";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "User Name:";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(17, 76);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(138, 20);
+            this.userNameTextBox.TabIndex = 5;
+            this.userNameTextBox.Text = "beluga@gregfutia.com";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(169, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Password:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(172, 76);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(142, 20);
+            this.passwordTextBox.TabIndex = 6;
+            this.passwordTextBox.Text = "Raman12!";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Subject";
+            // 
+            // subjectTextBox
+            // 
+            this.subjectTextBox.Location = new System.Drawing.Point(17, 171);
+            this.subjectTextBox.Name = "subjectTextBox";
+            this.subjectTextBox.Size = new System.Drawing.Size(297, 20);
+            this.subjectTextBox.TabIndex = 0;
+            // 
+            // SendSTMP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 299);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.subjectTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.recipentTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serverTextBox);
+            this.Controls.Add(this.sendEmail);
+            this.Name = "SendSTMP";
+            this.Text = "SendSTMP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox recipentTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverTextBox;
+        private System.Windows.Forms.Button sendEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox subjectTextBox;
+    }
+}
+
---
+++ Libraries/Lab.Communications/Lab.Communications/SendSTMP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+
+
+namespace Lab.Communications
+{
+    public partial class SendSTMP : Form
+    {
+        public SendSTMP()
+        {
+            InitializeComponent();
+        }
+
+        private void sendEmail_Click(object sender, EventArgs e)
+        {
+            SendMessage(subjectTextBox.Text, messageTextBox.Text);
+        }
+
+        public void SendMessage(String subject, String message)
+        {
+            SmtpClient smtpClient = new SmtpClient(serverTextBox.Text, 26);
+            NetworkCredential nc = new NetworkCredential(userNameTextBox.Text, passwordTextBox.Text);
+
+            smtpClient.Credentials = nc;
+
+            smtpClient.Send(userNameTextBox.Text, recipentTextBox.Text,
+                subject, message);
+            
+        }
+
+    }
+}
